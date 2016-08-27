3a3a14373ce63c87350a949bd9c8246a7b593a9a
Carol Silva <Carol@carols-air.attlocal.net>
2016-08-27 10:57:52 -0700

    add questions to verify if vampire or not

---
+++ ruby/vampires.rb
@@ -0,0 +1,8 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you? What year were you born?"
+age = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+answer = gets.chomp
+puts "Whould you like to enroll in the company's health insurance? (y/n)"
+enroll = gets.chomp
