926c8d9c43647c9bed532abd61fb6a4ee01959ab
Carol Silva <Carol@Carols-Air.attlocal.net>
2016-08-28 23:55:10 -0700

    add loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,31 +1,45 @@
+puts "How many employees will be processed?"
+input = gets.to_i
+until input == 0
 puts "What is your name?"
 name = gets.chomp
+	if name == "Drake Cula"
+		is_a_vampire = true
+	elsif name == "Tu Fang"
+		is_a_vampire = true
+	else
+		is_a_vampire = false
+	end
+	
 puts "How old are you? What year were you born?"
 age = gets.to_i
 year_born = gets.to_i
-if age == (2016 - year_born)
-	is_a_vampire = false
-else
-	is_a_vampire = true
-end
+	if age == (2016 - year_born)
+		is_a_vampire = false
+	else
+		is_a_vampire = true
+	end
+input = input - 1
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 order_garlic_bread = gets.chomp
-if order_garlic_bread == "y"
-	is_a_vampire = false
-else order_garlic_bread == "n"
-	is_a_vampire = true
-end
+	if order_garlic_bread == "y"
+		is_a_vampire = false
+	else order_garlic_bread == "n"
+		is_a_vampire = true
+	end
 
 puts "Whould you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp
-if insurance == "y"
-	is_a_vampire = false
-else insurance == "n"
-	is_a_vampire = true
-end
+	if insurance == "y"
+		is_a_vampire = false
+	else insurance == "n"
+		is_a_vampire = true
+	end
 
-if name = "Drake Cula" || "Tu Fang"
+if name == "Drake Cula"
+	puts "Definitely a vampire"
+elsif name =="Tu Fang"
 	puts "Definitely a vampire"
 elsif (order_garlic_bread == "n") && (insurance == "n")
 	puts "Probably a vampire"
@@ -35,18 +49,8 @@ elsif (insurance == "y") && (order_garlic_bread == "y")
 	puts "Probably not a vampire"
 else 
 	puts "Results inconclusive"
-		
+
+end		
 end
 
 	
-
-
-#wolves_like_sunshine = true
-#wolves_like_garlic = true
-#vampires_like_sunshine = false
-#vampires_like_garlic = false
- 
- #def not_vampire
-	#if (age <= 100 && garlic_bread == 'y') || insurance == 'y'
-	#puts "Probably not a vampire"
-#  end
