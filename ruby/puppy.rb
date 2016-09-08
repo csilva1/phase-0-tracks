b83ee904d397c5fb724aa69395801e2f04510108
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-08 15:13:40 -0700

    Complete puppy instances

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+
+#Species---------
+#Puppy Panda
+
+#Characteristics:
+#-friendly: yes
+#-name: varies
+#-color: black and white
+#-size: small - medium
+
+#Behavior:
+#-Cuddler
+#-Eater 
+#-Playful
