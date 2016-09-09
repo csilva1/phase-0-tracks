a7cddfb381ab49f5f8ea4e8d49fde69773ebcd6d
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-09 10:17:51 -0700

    Add Speak method, trick method with driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,25 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak(times)
+  	puts "Woof!" * times
+  end
 
+  def roll_over
+  	puts "*rolls over*"
+  end
+  def dog_years(human_years)
+  	dog_years = human_years * 7
+  end
+  def play_dead
+  	puts "*plays dead*"
+  end
 end
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(2)
+puppy.play_dead
 
