c918960d5b06b131a69e9b1e8f99d91ed0c386c6
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-09 10:50:08 -0700

    Add karate class with methods and random student name

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -18,6 +18,10 @@ class Puppy
   def play_dead
   	puts "*plays dead*"
   end
+  
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
 end
 puppy = Puppy.new
 puppy.fetch("ball")
@@ -25,4 +29,34 @@ puppy.speak(3)
 puppy.roll_over
 puppy.dog_years(2)
 puppy.play_dead
+
+
+class Karate
+	def round_house
+		puts "#{@name} *round house*"
+	end
+	def upper_cut
+		puts "#{@name} *upper cut*"
+	end
+	def initialize(name)
+		@name = name
+	end
+end
+
+name_list = ["Chuck Norris", "Zina Goucher", "Sharron Hirata", "Kourtney Shuey", "Carmelo Mallet", "Bruce Lee", "Bryant Cosgrove", "Manuel Fillers", "Booker Yearout", "Maya Cully"]
+
+karate_students = []
+50.times do |i|
+	karate_students << Karate.new(name_list.sample)
+end
+karate_students.each do |student|
+	student.round_house
+	student.upper_cut
+	
+end
+
+
+
+
+
 
