b6115c7e9ff38212324ea75800efb62cd55de842
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-10 18:56:33 -0700

    Add method to make santa turn 1 yr old and add argument to move bad reindeer to the back of the array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,9 +4,9 @@
 # 3rd. instance: 'initialize' create a method that PRINTS 'Initilaizing Santa instance...'
 
 class Santa
-	
+
 	def speak
-		print "Ho, ho, ho! Haaaaapppy holidays!"	# Whatever you want done
+		print "Ho, ho, ho! Haaaaapppy holidays!"
 	end
 	
 	def eat_milk_and_cookies(type)
@@ -17,19 +17,75 @@ class Santa
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		print " Initializing #{@gender} #{@ethnicity} Santa instance..."
+		# random_age = (0..140).to_a
+		# @age = random_age.sample
+		@age = 0
+		@reindeer_ranking = default_ranking
+		print #{@reindeer_ranking}
+		print " Initializing sex: #{@gender} ethnicity: #{@ethnicity} who is #{@age} yrs old and accomanied by #{@reindeer_ranking} Santa instance..."
 	end
 	
+	def celebrate_birthday
+		@age += 1
+		puts "Santa just turned #{@age}!"
+	end
+	
+	def get_mad_at(bad_reindeer)
+		deleted_reindeer = @reindeer_ranking.delete(bad_reindeer)
+		@reindeer_ranking << bad_reindeer
+	end
+
+	def default_ranking
+		["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+	#getter method
 end
 
-santa = Santa.new("female", "Mexican")
-santa.speak
-santa.eat_milk_and_cookies("snickerdoodle")
+santas = Santa.new("female", "Latina")
+santas.speak
+santas.eat_milk_and_cookies("snickerdoodle")
 
 
-santa = Santa.new("unicorn", "Puerto Rican")
-santa.speak
-santa.eat_milk_and_cookies("macadamiun nut cookie")
+santas = Santa.new("unicorn", "Asian")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
+
+santas = Santa.new("male", "black")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
+
+santas = Santa.new("mermaid", "Latino")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
+
+santas.get_mad_at("Vixen")
+
+santas.celebrate_birthday
+
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# random_age = (0..140).to_a
+# santa_age = []
+# 3.times do |i|
+# 	santa_age << Santa.new(random_age.sample)
+# end
+
 
 
 
