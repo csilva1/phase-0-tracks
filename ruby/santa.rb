57782e766b4d3a4e5ac5326cf3ec0756f4611b7a
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-11 14:16:21 -0700

    Create loop for new Santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,7 +4,8 @@
 # 3rd. instance: 'initialize' create a method that PRINTS 'Initilaizing Santa instance...'
 
 class Santa
-	# attr_reader :age, :ethnicity, :gender =>not needed because there is attr_accessor which makes it writable and readable
+	attr_reader :example_gender, :exampl_ethnicities
+	#=>not needed because there is attr_accessor which makes it writable and readable
 	attr_accessor :age, :ethnicity, :gender
 	def speak
 		print "Ho, ho, ho! Haaaaapppy holidays!"
@@ -42,102 +43,58 @@ class Santa
 	def default_ranking
 		["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
-	# #getter method
-	# def age
-	# 	@age
-	# end
 	
-	# def ethnicity
-	# 	@ethnicity
-	# end
+	def santa_info
+		puts "This Santa is #{@gender}, and #{@ethnicity}"
+	end
 	
-	# def gender
-	# 	@gender
-	# end
-	# #setter method
-	# def gender=(new_gender)
-	# 	@gender = new_gender
-	# end
-	# def age=(new_age)
-	# 	@age = new_age
-	# end
-	# def ethnicity=(new_ethnicity)
-	# 	@ethnicity = new_ethnicity
-	# end
 end
 
-santas = Santa.new("female", "Latina")
-santas.speak
-santas.eat_milk_and_cookies("snickerdoodle")
+# santas = Santa.new("female", "Latina")
+# santas.speak
+# santas.eat_milk_and_cookies("snickerdoodle")
 
 
-santas = Santa.new("unicorn", "Asian")
-santas.speak
-santas.eat_milk_and_cookies("macadamiun nut cookie")
+# santas = Santa.new("unicorn", "Asian")
+# santas.speak
+# santas.eat_milk_and_cookies("macadamiun nut cookie")
 
-santas = Santa.new("male", "black")
-santas.speak
-santas.eat_milk_and_cookies("macadamiun nut cookie")
+# santas = Santa.new("male", "black")
+# santas.speak
+# santas.eat_milk_and_cookies("macadamiun nut cookie")
 
-santas = Santa.new("mermaid", "Latino")
-santas.speak
-santas.eat_milk_and_cookies("macadamiun nut cookie")
+# santas = Santa.new("mermaid", "Latino")
+# santas.speak
+# santas.eat_milk_and_cookies("macadamiun nut cookie")
 
 # santas.age = "Santa just had his birthday and is now 1 yrs old!"
 
-santas.get_mad_at("Vixen")
+# santas.get_mad_at("Vixen")
 
-santas.celebrate_birthday
+# santas.celebrate_birthday
 
-santas.gender = "The Latina santa that was a female"
-puts "#{santas.gender} is now a male."
+# santas.gender = "The Latina santa that was a female"
+# puts "#{santas.gender} is now a male."
 
-santas.age = 3
-puts "Santa is now #{santas.age}"
+# santas.age = 3
+# puts "Santa is now #{santas.age}"
 
-santas.ethnicity = "The Santa that was formerly Latina is now a unicorn"
-puts "Santa's can do more than bring presents, they can also transform. #{santas.ethnicity}"
+# santas.ethnicity = "The Santa that was formerly Latina is now a unicorn"
+# puts "Santa's can do more than bring presents, they can also transform. #{santas.ethnicity}"
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-
-santas = []
+ 
+ 
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+
+puts "how many Santa's would you want to create?"
+how_many = gets.to_i
+
+how_many.times do
+
+	Santa.new(example_genders.sample, example_ethnicities.sample).santa_info
+
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# random_age = (0..140).to_a
-# santa_age = []
-# 3.times do |i|
-# 	santa_age << Santa.new(random_age.sample)
-# end
-
-
-
 
 
 
