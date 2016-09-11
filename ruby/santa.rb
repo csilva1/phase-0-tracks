82edb29abe491a3432179ad28c41021cfe42e998
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-10 21:36:59 -0700

    Add driver code to setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -24,7 +24,10 @@ class Santa
 		print #{@reindeer_ranking}
 		print " Initializing sex: #{@gender} ethnicity: #{@ethnicity} who is #{@age} yrs old and accomanied by #{@reindeer_ranking} Santa instance..."
 	end
-	
+
+	def age
+		@age = 0
+	end
 	def celebrate_birthday
 		@age += 1
 		puts "Santa just turned #{@age}!"
@@ -54,6 +57,12 @@ class Santa
 	def gender=(new_gender)
 		@gender = new_gender
 	end
+	def age=(new_age)
+		@age = new_age
+	end
+	def ethnicity=(new_ethnicity)
+		@ethnicity = new_ethnicity
+	end
 end
 
 santas = Santa.new("female", "Latina")
@@ -61,17 +70,17 @@ santas.speak
 santas.eat_milk_and_cookies("snickerdoodle")
 
 
-# santas = Santa.new("unicorn", "Asian")
-# santas.speak
-# santas.eat_milk_and_cookies("macadamiun nut cookie")
+santas = Santa.new("unicorn", "Asian")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
 
-# santas = Santa.new("male", "black")
-# santas.speak
-# santas.eat_milk_and_cookies("macadamiun nut cookie")
+santas = Santa.new("male", "black")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
 
-# santas = Santa.new("mermaid", "Latino")
-# santas.speak
-# santas.eat_milk_and_cookies("macadamiun nut cookie")
+santas = Santa.new("mermaid", "Latino")
+santas.speak
+santas.eat_milk_and_cookies("macadamiun nut cookie")
 
 # santas.age = "Santa just had his birthday and is now 1 yrs old!"
 
@@ -82,49 +91,11 @@ santas.celebrate_birthday
 santas.gender = "The Latina santa that was a female"
 puts "#{santas.gender} is now a male."
 
-# end
+santas.age = 3
+puts "Santa is now #{santas.age}"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# random_age = (0..140).to_a
-# santa_age = []
-# 3.times do |i|
-# 	santa_age << Santa.new(random_age.sample)
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+santas.ethnicity = "The Santa that was formerly Latina is now a unicorn"
+puts "Santa's can do more than bring presents, they can also transform. #{santas.ethnicity}"
 
 
 
