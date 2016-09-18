900c1978da2eb185b6c9a01f5d274a457316a7f4
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-17 17:03:17 -0700

    Create pseudocode to sort array of words from longest to shortest

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// create an array that contains strings, and make sure to put one string that is longer
+// than the other.
+// then the longest word of the array needs to be returned do the following:
+// the strings in the array needs to be counted
+// then assort the array from longest to shortest
+// then the longest word should be placed in index 0
+// then print index 0, because the longest word should be there
