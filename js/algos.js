0a5574fbc245f5a5866773c8dde0f0225b2f73b4
Carol Silva <Carol@carols-air.attlocal.net>
2016-09-19 00:13:38 -0700

    Add if/else to get the words in array and check if its longer than the other words in array and to switch

--- js/algos.js
+++ js/algos.js
@@ -5,3 +5,33 @@
 // then assort the array from longest to shortest
 // then the longest word should be placed in index 0
 // then print index 0, because the longest word should be there
+	function longestWord() {
+	var list = ["long", "longer", "longest"];
+
+for (var i=0; i<list.length; i++) {
+    console.log(list[i]);
+}
+    var word1 = list[0].length;
+    console.log(word1);
+    var word2 = list[1].length;
+    console.log(word2);
+    var word3 = list[2].length;
+    console.log(word3);
+    if(word1 < word2) {
+       return (word2 = word1);
+    } else if(word2 < word1) {
+    	return (word1 = word2);
+    } else if(word2 < word3) {
+       return (word3 = word2);
+    } else if(word3 < word2) {
+    	return (word2 = word3);
+    } else if(word1 < word3) {
+    	return (word3 = word1);
+    } else if(word3 < word1); {
+    	return (word1 = word3);
+    }  
+    		
+    	}
+    	
+    	console.log (longestWord.list)
+    
