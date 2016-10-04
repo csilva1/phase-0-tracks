5652a6af762fb554f411462ce3fb27ba0d9e0276
Carol Silva <Carol@carols-air.attlocal.net>
2016-10-04 12:14:28 -0700

    Create constructor functions and call it

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,15 +1,36 @@
-var horses = {},
-	i,
-	colors = ["blue", "red", "pink", "purple"],
-	names = ["Ed", "Howard", "Bob", "Janet"];
-colors.push("green")
-names.push("Martha")
+// var horses = {},
+// 	i,
+// 	colors = ["blue", "red", "pink", "purple"],
+// 	names = ["Ed", "Howard", "Bob", "Janet"];
+// colors.push("green")
+// names.push("Martha")
 
-for (i = 0; i < names.length; i++) {
-   	horses[names[i]] = colors[i];
+// for (i = 0; i < names.length; i++) {
+//    	horses[names[i]] = colors[i];
+// }
+
+// console.log (horses)
+
+
+function Car(year, color, isAutomatic){
+	
+	console.log ("Car types:", this);
+	this.year = year;
+	this.color = color;
+	this.isAutomatic = isAutomatic;
+	this.sound = function() { console.log ("Vroom!"); };
+
 }
 
-console.log (horses)
+var firstCar = new Car(2016, "red", true)
+console.log(firstCar);
+console.log("*car starts*")
+firstCar.sound();
+
+var secondCar = new Car(1999, "blue", false)
+console.log(secondCar);
+console.log("*car starts*")
+secondCar.sound();
 
 
 
