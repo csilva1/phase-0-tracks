e418d8324835516ef6e8a36ac13e7efed80147e1
Carol Silva <Carol@carols-air.attlocal.net>
2016-10-04 11:41:57 -0700

    Create 2 arrays and merge into object

---
+++ js/data_structures.js
@@ -0,0 +1,17 @@
+var horses = {},
+	i,
+	colors = ["blue", "red", "pink", "purple"],
+	names = ["Ed", "Howard", "Bob", "Janet"];
+colors.push("green")
+names.push("Martha")
+
+for (i = 0; i < names.length; i++) {
+   	horses[names[i]] = colors[i];
+}
+
+console.log (horses)
+
+
+
+
+
